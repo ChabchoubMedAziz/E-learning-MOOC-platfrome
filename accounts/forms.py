2b26c5ea956c9d863c234ad8fe4d7d2f3996ca8d
khalil bouzoffara <khalil.bouzoffara@ensi-uma.tn>
2022-05-01 02:01:34 +0100

    some sign up changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,6 +22,38 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(UserCreationForm):
+
+    gen = [
+        ('m','Male'),
+        ('f','Female'),
+    ]
+
+    ctry = [
+        ('Australia','Australia'),
+        ('Bangladesh','Bangladesh'),
+        ('Brazil','Brazil'),
+        ('Canada','Canada'),
+        ('China','China'),
+        ('France','France'),
+        ('Germany','Germany'),
+        ('India','India'),
+        ('Italy','Italy'),
+        ('Mexico','Mexico'),
+        ('Pakistan','Pakistan'),
+        ('Spain','Spain'),
+        ('Tunisia','Tunisia'),
+        ('United Kingdom','United Kingdom'),
+        ('United States','United States'),
+        ('Other','Other'),
+    ]
+
+    stt = [
+        ("Bachelor's","Bachelor's"),
+        ("Doctorate","Doctorate"),
+        ("Master's","Master's"),
+        ("Secondary","Secondary"),
+    ]
+
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -51,8 +83,57 @@ class SignUpForm(UserCreationForm):
         )
     )
 
+    YoB = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                "class": "form-control"
+            },
+        format='%Y-%m-%d',     
+        )
+    )
+
+
+    gender = forms.ChoiceField(
+        
+        widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            }
+        ),
+        required=True,
+        error_messages={'required':'Please entre your gender info', 'invalid_choice':'please select a valid choice'},
+        choices=gen,
+
+    )
+    
+    country = forms.ChoiceField(
+        
+        widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            }
+        ),
+        required=True,
+        error_messages={'required':'Please entre your country info', 'invalid_choice':'please select a valid choice'},
+        choices=ctry,
+
+    )
+
+    state = forms.ChoiceField(
+        
+        widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            }
+        ),
+        required=True,
+        error_messages={'required':'Please entre your state info', 'invalid_choice':'please select a valid choice'},
+        choices=stt,
+
+    )
+
 
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'is_teacher', 'is_student')
+        fields = ('username', 'email', 'password1', 'password2', 'YoB','gender' , 'country', 'state', 'is_teacher', 'is_student')
--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,45 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+
+	gen = [
+        ('m','Male'),
+        ('f','Female'),
+    ]
+	
+	ctry = [
+        ('Australia','Australia'),
+        ('Bangladesh','Bangladesh'),
+        ('Brazil','Brazil'),
+        ('Canada','Canada'),
+        ('China','China'),
+        ('France','France'),
+        ('Germany','Germany'),
+        ('India','India'),
+        ('Italy','Italy'),
+        ('Mexico','Mexico'),
+        ('Pakistan','Pakistan'),
+        ('Spain','Spain'),
+        ('Tunisia','Tunisia'),
+        ('United Kingdom','United Kingdom'),
+        ('United States','United States'),
+        ('Other','Other'),
+    ]
+
+	stt = [
+        ("Bachelor's","Bachelor's"),
+        ("Doctorate","Doctorate"),
+        ("Master's","Master's"),
+        ("Secondary","Secondary"),
+    ]
+
+	YoB = models.DateField("Year of Birth (yyyy-mm-dd)",auto_now_add=False, auto_now=False, blank=True, null=True)
+	gender = models.CharField(max_length=30,blank=True, null=True, choices=gen)
+	country = models.CharField(max_length=30,blank=True, null=True, choices=ctry)
+	state = models.CharField(max_length=30,blank=True, null=True, choices=stt)
 	is_teacher = models.BooleanField('Is Teacher', default=False)
 	is_student = models.BooleanField('Is Student', default=False)
+
 """class Account(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	avatar = models.ImageField(upload_to='avatar/', default='avatar_1.png')
